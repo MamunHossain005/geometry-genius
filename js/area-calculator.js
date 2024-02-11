6ce91a025a5b8f9ddcb08b46d8750ea50a6590c0
Md-Mamun-Hossain <hossainmamun624@gmail.com>
2024-02-11 20:53:43 +0600

    final commit

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -11,6 +11,9 @@ function calculateTriangleArea() {
     setTextById('triangle-area', area);
     document.getElementById('triangle-base').value = '';
     document.getElementById('triangle-height').value = '';
+    if(!isNaN(area)) {
+        addToCalculationEntry('Triangle', area);
+    }
 }
 
 function calculateRectangleArea() {
@@ -26,4 +29,28 @@ function calculateRectangleArea() {
     setTextById('rectangle-area', area);
     document.getElementById('rectangle-length').value = '';
     document.getElementById('rectangle-breadth').value = '';
+
+    if(!isNaN(area)) {
+        addToCalculationEntry('Rectangle', area);
+    }
 }
+
+function calculateParallelogramArea() {
+    //get parallelogram base value
+    const base = getValueById('parallelogram-base');
+
+    //get parallelogram height value
+    const height = getValueById('parallelogram-height');
+
+    const area = base * height;
+
+    //show triangle area
+    setTextById('parallelogram-area', area);
+    document.getElementById('parallelogram-base').value = '';
+    document.getElementById('parallelogram-height').value = '';
+
+    if(!isNaN(area)) {
+        addToCalculationEntry('Parallelogram', area);
+    }
+}
+
--- js/utilities.js
+++ js/utilities.js
@@ -19,3 +19,31 @@ function setTextById(id, text) {
         field.innerText = text;
     }
 }
+
+/**
+ * 1.get the element where you want to add the dynamic HTML
+ * 2.create an element you want to add
+ * 3.if needed add some class
+ * 4.set inner HTML. it could be dynamic Template string
+ * 5.append the created element as a child of the parent
+ */
+function addToCalculationEntry(areaType, area) {
+    const calculationEntry = document.getElementById('calculation-entry');
+    const p = document.createElement('p');
+    const count = calculationEntry.childElementCount;
+    p.classList.add('my-4');
+    p.innerHTML = `${count + 1}. ${areaType} <span>${area}</span><span>cm</span><sup>2</sup> <button class = "btn btn-primary btn-convert">Convert to m<sup>2</sup>`;
+    calculationEntry.appendChild(p);
+
+    const convertElements = document.getElementsByClassName('btn-convert');
+    for(let el of convertElements) {
+        el.addEventListener('click', (event)=> {
+            const parent = event.target.parentNode;
+            const nodes = parent.childNodes;
+            const changeArea = nodes[1];
+            changeArea.innerText = parseFloat(changeArea.innerText) / 100;
+            const cm = nodes[2];
+            cm.innerText = 'm';
+        });
+    }
+}
